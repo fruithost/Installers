0ab7724c2522b896362fbebfa6be7f8423b5511a
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2023-06-06 21:21:34 +0200

    Adding first working installer

--- debian.sh
+++ debian.sh
@@ -113,6 +113,136 @@ install_rsyslog() {
 	apt-get install -y rsyslog rsyslog-mysql 
 }
 
+create_directorys() {
+	mkdir /etc/fruithost
+	mkdir /var/fruithost/logs
+	mkdir /var/fruithost/users
+}
+
+password_generate() {
+	set="abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	n=28
+	rand=""
+	
+	for i in `seq 1 $n`; do
+		char=${set:$RANDOM % ${#set}:1}
+		rand+=$char
+	done
+	
+	echo $rand
+}
+
+fruithost_fetch() {
+	# Grab latest versions
+	git clone https://github.com/fruithost/Panel.git /etc/fruithost/panel
+	git clone https://github.com/fruithost/Binary.git /etc/fruithost/bin
+	git clone https://github.com/fruithost/Config.git /etc/fruithost/config
+	git clone https://github.com/fruithost/Themes.git /etc/fruithost/themes
+	git clone https://github.com/fruithost/Placeholder.git /etc/fruithost/placeholder
+	git clone https://github.com/fruithost/Modules.git /etc/fruithost/modules
+	
+	# Modify permissions
+	chmod 0777 /etc/fruithost/bin/cli.php
+	chmod 0777 /etc/fruithost/bin/cronjob
+	chmod 0777 /etc/fruithost/bin/fruithost.sh
+	
+	# Adding global scripts
+	ln -s /etc/fruithost/bin/fruithost.sh /usr/bin/fruithost
+	ln -s /etc/fruithost/bin/fruithost.sh /usr/local/bin/fruithost
+}
+
+create_config() {
+	mysql_password=$(password_generate)
+	
+	# Delete old config
+	rm /etc/fruithost/.config.php
+	
+	echo "<?php" >> /etc/fruithost/.config.php
+	echo "	# Database Connection" >> /etc/fruithost/.config.php
+	echo "	define('DATABASE_HOSTNAME',		'localhost');" >> /etc/fruithost/.config.php
+	echo "	define('DATABASE_PORT',			3306);" >> /etc/fruithost/.config.php
+	echo "	define('DATABASE_USERNAME',		'fruithost');" >> /etc/fruithost/.config.php
+	echo "	define('DATABASE_PASSWORD',		'$mysql_password');" >> /etc/fruithost/.config.php
+	echo "	define('DATABASE_NAME',			'fruithost');" >> /etc/fruithost/.config.php
+	echo "	define('DATABASE_PREFIX',		'fh_');" >> /etc/fruithost/.config.php
+	echo "" >> /etc/fruithost/.config.php
+	echo "	# Hosting Path" >> /etc/fruithost/.config.php
+	echo "	define('HOST_PATH',			'/var/fruithost/users/');" >> /etc/fruithost/.config.php
+	echo "?>" >> /etc/fruithost/.config.php
+	
+	# Create MySQL User / Database
+	mysql --execute="CREATE DATABASE IF NOT EXISTS fruithost /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	mysql --execute="DROP USER fruithost@localhost;"
+	mysql --execute="CREATE USER fruithost@localhost IDENTIFIED BY '${mysql_password}';"
+	mysql --execute="GRANT ALL PRIVILEGES ON fruithost.* TO 'fruithost'@'localhost';"
+	mysql --execute="FLUSH PRIVILEGES;"
+	
+	# Delete old security
+	rm /etc/fruithost/.security.php
+	
+	mysql_salt=$(password_generate)
+	password_salt=$(password_generate)
+	encryption_salt=$(password_generate)
+	
+	echo "<?php" >> /etc/fruithost/.security.php
+	echo "	# DO NOT MODIFY, IT WILL BREAKS ALL YOUR DATA!" >> /etc/fruithost/.security.php
+	echo "" >> /etc/fruithost/.security.php
+	echo "	define('MYSQL_PASSWORTD_SALT',	'$mysql_salt');" >> /etc/fruithost/.security.php
+	echo "	define('RESET_PASSWORD_SALT',	'$password_salt');" >> /etc/fruithost/.security.php
+	echo "	define('ENCRYPTION_SALT',	'$encryption_salt');" >> /etc/fruithost/.security.php
+	echo "?>" >> /etc/fruithost/.security.php
+}
+
+update_config() {
+	admin_password=$(password_generate)
+	
+	# Apache2
+	ln -s /etc/fruithost/config/apache2/global.conf /etc/apache2/sites-available/global.conf
+	ln -s /etc/fruithost/config/apache2/panel.conf /etc/apache2/sites-available/panel.conf
+	
+	a2ensite global panel
+	a2dissite 000-default default-ssl
+	service apache2 reload
+	
+	# Set Hostname to ServerName my.fruit.host in /etc/apache2/sites-available/panel.conf
+	create_config
+	
+	# Import SQL
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_modules;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_modules (id int(11) NOT NULL AUTO_INCREMENT, name varchar(255) DEFAULT NULL, time_enabled datetime DEFAULT NULL ON UPDATE current_timestamp(), time_updated datetime DEFAULT NULL ON UPDATE current_timestamp(), state enum('ENABLED','DISABLED') DEFAULT 'DISABLED', time_deleted datetime DEFAULT NULL, PRIMARY KEY (id)) AUTO_INCREMENT=1;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_modules_settings;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_modules_settings (id int(11) NOT NULL AUTO_INCREMENT, module varchar(255) DEFAULT NULL, \`key\` varchar(255) DEFAULT NULL, value longtext DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_repositorys;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_repositorys (id int(11) NOT NULL AUTO_INCREMENT, url varchar(255) DEFAULT NULL,time_updated datetime DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"	
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_repositorys VALUES ('10', 'https://github.com/fruithost/Modules/', '2020-03-23 13:35:46');"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_settings;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_settings (id int(11) NOT NULL AUTO_INCREMENT, \`key\` varchar(255) DEFAULT NULL, value longtext DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_users;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_users (id int(11) NOT NULL AUTO_INCREMENT, username varchar(255) DEFAULT NULL, password varchar(255) DEFAULT NULL, email varchar(255) DEFAULT NULL, lost_enabled enum('YES','NO') DEFAULT 'NO', lost_time datetime DEFAULT NULL ON UPDATE current_timestamp(), lost_token varchar(255) DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_users_data;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_users_data (id int(11) NOT NULL AUTO_INCREMENT, user_id int(11) DEFAULT NULL, phone_number varchar(255) DEFAULT NULL, address varchar(255) DEFAULT NULL, name_first varchar(255) DEFAULT NULL, name_last varchar(255) DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_users_permissions;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_users_permissions (id int(11) NOT NULL AUTO_INCREMENT, user_id int(11) DEFAULT NULL, permission varchar(255) DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="DROP TABLE IF EXISTS fh_users_settings;"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="CREATE TABLE fh_users_settings (id int(11) NOT NULL AUTO_INCREMENT, user_id int(11) DEFAULT NULL, \`key\` varchar(255) DEFAULT NULL, value longtext DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;"
+
+	# Create Admin-Account
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_users VALUES ('1', 'admin', UPPER(SHA2(CONCAT('1', '${mysql_salt}', '${admin_password}'), 512)), 'admin@localhost', 'NO', '2019-05-11 12:35:14', null);"
+	
+	# Set Permissions
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_users_permissions VALUES (null, '1', 'USERS::VIEW');"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_users_permissions VALUES (null, '1', 'MODULES::VIEW');"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_users_permissions VALUES (null, '1', 'THEMES::VIEW');"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_users_permissions VALUES (null, '1', 'LOGFILES::VIEW');"
+	mysql --user="fruithost" --password="${mysql_password}" --database="fruithost" --execute="INSERT INTO fh_users_permissions VALUES (null, '1', 'SERVER::VIEW');"
+	
+	color "\e[39mThe Admin-Account was created. You can now login to:"
+	color "\e[39mURL: http://my.${HOSTNAME}/"
+	color "\e[39mUsername: admin"
+	color "\e[39mPassword: ${admin_password}"
+	color "\e[91mPlease change the password after the first login!"	
+}
+
 install_software() {
 	if [ "$EUID" -ne 0 ]
 	  then color "\e[91mPlease run as root"
@@ -165,14 +295,21 @@ install_software() {
 	
 
 	color "\e[36mStarting services..."
-	/etc/init.d/apache2 restart
+	service apache2 restart
 	color "\e[32m[OK]\e[39m WebServer"
-	/etc/init.d/mariadb restart
+	service mariadb restart
 	color "\e[32m[OK]\e[39m MySQL-Database"
-	/etc/init.d/proftpd restart
+	service proftpd restart
 	color "\e[32m[OK]\e[39m FTP-Service"
+	service php8.2-fpm start
+	color "\e[32m[OK]\e[39m PHP-FPM"
 }
 
-install_software
+#install_software
+	
+## Grab latest version fruithost files
+#create_directorys
+#fruithost_fetch
+update_config
 
 } # this ensures the entire script is downloaded #
